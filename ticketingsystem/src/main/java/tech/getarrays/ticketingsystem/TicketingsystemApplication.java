0f2d02687cc70a47d928c82a007747b41482962d
L.A.M.D. Perera <mokshikaperera98@gmail.com>
2022-10-27 03:51:47 +0530

    Bus Schedule Backend add, getone, getall completed

--- ticketingsystem/src/main/java/tech/getarrays/ticketingsystem/TicketingsystemApplication.java
+++ ticketingsystem/src/main/java/tech/getarrays/ticketingsystem/TicketingsystemApplication.java
@@ -8,7 +8,6 @@ public class TicketingsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketingsystemApplication.class, args);
-		System.out.println("12111111111111111111111111111111113");
 	}
 
 }
