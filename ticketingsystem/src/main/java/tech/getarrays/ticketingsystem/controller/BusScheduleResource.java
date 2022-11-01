64f4c7e933330791fc1d4369443408c63a1dc1bc
Supun Hansaka <100472049+IT20653118@users.noreply.github.com>
2022-11-01 21:00:09 +0530

    Controllers updated

--- ticketingsystem/src/main/java/tech/getarrays/ticketingsystem/controller/BusScheduleResource.java
+++ ticketingsystem/src/main/java/tech/getarrays/ticketingsystem/controller/BusScheduleResource.java
@@ -1,4 +1,4 @@
-package tech.getarrays.ticketingsystem;
+package tech.getarrays.ticketingsystem.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
