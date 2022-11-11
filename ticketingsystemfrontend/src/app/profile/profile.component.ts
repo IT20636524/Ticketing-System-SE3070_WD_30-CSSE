266f2dcf29900dbfc76bf04eba0a0d01641fcfa7
Supun Hansaka <100472049+IT20653118@users.noreply.github.com>
2022-11-11 21:43:40 +0530

    Profile html, component updated

--- ticketingsystemfrontend/src/app/profile/profile.component.ts
+++ ticketingsystemfrontend/src/app/profile/profile.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Passenger } from '../register/passenger';
-import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { PassengerService } from '../register/passenger.service';
 import { NgForm } from '@angular/forms';
+import { Account } from '../account/account';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-profile',
@@ -13,31 +15,64 @@ export class ProfileComponent implements OnInit {
   public passengers: Passenger[] = [];
   public viewPassenger!: Passenger;
   public editPassenger!: Passenger;
+  user !: Passenger;
+  id!: number;
+  fullName!: string;
+  email!: string;
+  mobile!: string;
+  password!: string;
 
-  constructor(private passengerService : PassengerService) { }
+  constructor(
+    private passengerService: PassengerService,
+    private accountService: AccountService,
+    private http: HttpClient) { }
+
 
   ngOnInit(): void {
     this.getPassengers();
   }
 
-  public getPassengers() : void {
+
+  public getPassengers(): void {
     this.passengerService.getPassengers().subscribe(
-      (response : Passenger[]) => {
-        this.passengers = response;
+      (response: Passenger[]) => {
+        this.user = response[0];
+        this.id = this.user.id;
+        this.email = this.user.email;
+        this.fullName = this.user.fullName;
+        this.mobile = this.user.mobile;
+        this.password = this.user.password;
+        console.log(response)
       },
-      (error : HttpErrorResponse) => {
+      (error: HttpErrorResponse) => {
         alert(error.message);
       }
     );
   }
-  public onUpdatePassenger(passenger : Passenger) : void{
+  public onUpdatePassenger(passenger: Passenger): void {
+    if (this.id) {
+      passenger.id = this.id;
+    }
     this.passengerService.updatePassenger(passenger).subscribe(
       (response: Passenger) => {
-        console.log(response);
-        // this.getPassengers();
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
+      }
+    );
+  }
+
+  public onAddCredit(creditForm: NgForm): void {
+    document.getElementById('credit-form')?.click();
+    this.accountService.addAccount(creditForm.value).subscribe(
+      (response: Account) => {
+        console.log(response);
+        alert("Credit added successfully");
+        creditForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        creditForm.reset();
       }
     );
   }
