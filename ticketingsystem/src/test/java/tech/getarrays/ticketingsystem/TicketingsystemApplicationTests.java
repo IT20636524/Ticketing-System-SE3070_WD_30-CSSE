14c12a6da40f6afeb133c4ef54c62d7c54964cb6
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-11-11 12:44:26 +0530

    Test cases

--- ticketingsystem/src/test/java/tech/getarrays/ticketingsystem/TicketingsystemApplicationTests.java
+++ ticketingsystem/src/test/java/tech/getarrays/ticketingsystem/TicketingsystemApplicationTests.java
@@ -1,13 +1,40 @@
 package tech.getarrays.ticketingsystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tech.getarrays.ticketingsystem.model.Passenger;
+import tech.getarrays.ticketingsystem.repo.PassengerRepo;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class TicketingsystemApplicationTests {
 
+	@Autowired
+	PassengerRepo passengerRepo;
+
 	@Test
-	void contextLoads() {
+	public void testCreate(){
+		Passenger passenger=new Passenger();
+		passenger.setId(6L);
+		passenger.setEmail("sachin@test.com");
+		passenger.setFullName("Sachin");
+		passenger.setImageUrl("https://bootdey.com/img/Content/avatar/avatar3.png");
+		passenger.setMobile("0778855666");
+		passenger.setNic("997055242");
+		passenger.setPassengerCode("aaf8142e-e0b3-4da1-b097-30fea1ab775b");
+		passengerRepo.save(passenger);
+		assertNotNull(passengerRepo.findPassengerById(5L).get());
 	}
 
+	@Test
+	public void testReadAll(){
+		List<Passenger> list = passengerRepo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+	}
 }
