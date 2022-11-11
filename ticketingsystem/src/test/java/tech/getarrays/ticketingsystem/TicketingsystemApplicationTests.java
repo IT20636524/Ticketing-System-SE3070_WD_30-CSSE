ce6fbcdfec6ddd7cab02bc5aea239bc0b6ff239f
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-11-11 15:14:04 +0530

    Update TicketingsystemApplicationTests.java

--- ticketingsystem/src/test/java/tech/getarrays/ticketingsystem/TicketingsystemApplicationTests.java
+++ ticketingsystem/src/test/java/tech/getarrays/ticketingsystem/TicketingsystemApplicationTests.java
@@ -3,7 +3,11 @@ package tech.getarrays.ticketingsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tech.getarrays.ticketingsystem.model.BusSchedule;
+import tech.getarrays.ticketingsystem.model.Fares;
 import tech.getarrays.ticketingsystem.model.Passenger;
+import tech.getarrays.ticketingsystem.repo.BusScheduleRepo;
+import tech.getarrays.ticketingsystem.repo.FaresRepo;
 import tech.getarrays.ticketingsystem.repo.PassengerRepo;
 
 import java.util.List;
@@ -17,9 +21,13 @@ class TicketingsystemApplicationTests {
 
 	@Autowired
 	PassengerRepo passengerRepo;
+	@Autowired
+	FaresRepo faresRepo;
+	@Autowired
+	BusScheduleRepo busScheduleRepo;
 
 	@Test
-	public void testCreate(){
+	public void testCreatePassenger(){
 		Passenger passenger=new Passenger();
 		passenger.setId(6L);
 		passenger.setEmail("sachin@test.com");
@@ -33,7 +41,7 @@ class TicketingsystemApplicationTests {
 	}
 
 	@Test
-	public void testReadAll(){
+	public void testReadAllPassengers(){
 		List<Passenger> list = passengerRepo.findAll();
 		assertThat(list).size().isGreaterThan(0);
 	}
@@ -55,4 +63,46 @@ class TicketingsystemApplicationTests {
 		Optional<Passenger> passenger = passengerRepo.findPassengerById(5L);
 		assertFalse(passenger.isEmpty());
 	}
+
+	@Test
+	public void testReadAllFares(){
+		List<Fares> list = faresRepo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+	}
+
+//	@Test
+//	public void testAddFares(){
+//		Fares fares=new Fares();
+//		fares.setId(5L);
+//		fares.setRouteNo("009996");
+//		fares.setBusNo("tv3443");
+//		fares.setTimePeriod("8.00-12.00");
+//		fares.setNoOfPassengers("200");
+//		fares.setTotalFare("60000");
+//		fares.setDate("2022-08-20");
+//		faresRepo.save(fares);
+//		assertNotNull(faresRepo.findFareById(5L));
+//	}
+
+	@Test
+	public void testReadAllBusSchedules(){
+		List<BusSchedule> list = busScheduleRepo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+	}
+
+	@Test
+	public void testAddBusSchedules(){
+		BusSchedule busSchedule=new BusSchedule();
+		busSchedule.setId(7L);
+		busSchedule.setStartLocation("Colombo");
+		busSchedule.setArrivingLocation("Balangoda");
+		busSchedule.setDate("27-10-2022");
+		busSchedule.setTimePeriod("8.00-12-00");
+		busSchedule.setBusNo("tv3333");
+		busScheduleRepo.save(busSchedule);
+		assertNotNull(busScheduleRepo.findBusScheduleById(7L));
+
+	}
+
+
 }
